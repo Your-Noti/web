2db0e02f944fc14e6d2e78918869d9b47b7bfe41
Nikita Kurochka <Freenikita95@gmail.com>
2022-02-19 00:41:23 +0200

    update actions

--- src/index.js
+++ src/index.js
@@ -12,27 +12,29 @@ import reportWebVitals from './reportWebVitals';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCIbVetoNZ3rEXlVjiPxI6MeFrwp1dzH6c",
-//     authDomain: "notification-mob.firebaseapp.com",
-//     databaseURL: "https://notification-mob-default-rtdb.europe-west1.firebasedatabase.app",
-//     projectId: "notification-mob",
-//     storageBucket: "notification-mob.appspot.com",
-//     messagingSenderId: "136916103479",
-//     appId: "1:136916103479:web:1e6cf618488c3aa3d2aab2",
-//     measurementId: "G-YW5WE2WGX3"
-//
-// };
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_API_KEY,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
+console.log('firebaseConfig', firebaseConfig)
+console.log('process', process.env)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
