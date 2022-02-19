b2b900cad1d7a644f94c946356d9b510e4cdb91d
Nikita Kurochka <Freenikita95@gmail.com>
2022-02-20 00:22:03 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './servises/firebase'
 
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
 
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_FIREBASE_API_KEY,
-//     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-// };
+const getRootElement = document.getElementById('root')
 
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App/>, getRootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
