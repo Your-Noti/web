5873ba7d41abede4eaebebbc8cecc319764a9914
Nikita Kurochka <Freenikita95@gmail.com>
2022-02-21 11:10:58 +0200

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './servises/firebase'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./servises/firebase";
 
+const getRootElement = document.getElementById("root");
 
-const getRootElement = document.getElementById('root')
-
-ReactDOM.render(<App/>, getRootElement);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  getRootElement
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
