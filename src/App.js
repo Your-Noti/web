0bc09e838929a95f83f3172f0feacb7bfd69b78c
Nikita Kurochka <Freenikita95@gmail.com>
2022-02-22 11:25:42 +0200

    add eslint

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ const AppBara = () => (
         </IconButton>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           News
+
         </Typography>
         <Button color="inherit">Login</Button>
       </Toolbar>
